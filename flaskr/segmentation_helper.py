3f997a390c2b1c991a4b90667e479db4ea3e19aa
Lennart Reinke <lennartreinke@yahoo.de>
2024-10-30 12:43:12 +0100

    Fixing linking issue to the new model version.

--- flaskr/segmentation_helper.py
+++ flaskr/segmentation_helper.py
@@ -94,8 +94,8 @@ def predictSegments(image, coordinates, labels):
     from sam2.build_sam import build_sam2
     from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-    sam2_checkpoint = "./checkpoints/sam2_hiera_large.pt"
-    model_cfg = "sam2_hiera_l.yaml"
+    sam2_checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "configs/sam2.1/sam2.1_hiera_l"
 
     sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
 
